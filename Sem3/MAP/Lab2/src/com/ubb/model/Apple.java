818ea01245445826ee8e42ffd1be599f6feb7c4f
Diana Noveanu <diana.noveanu@teleporthq.io>
2019-10-02 15:54:39 +0300

    stuff

---
+++ Sem3/MAP/Lab2/src/com/ubb/model/Apple.java
@@ -0,0 +1,11 @@
+package com.ubb.model;
+
+public class Apple implements Object {
+    public Integer getWeight() {
+        return null;
+    }
+
+    public String getObjClass() {
+        return null;
+    }
+}
---
+++ Sem3/MAP/Lab2/src/com/ubb/model/Book.java
@@ -0,0 +1,4 @@
+package com.ubb.model;
+
+public class Book {
+}
